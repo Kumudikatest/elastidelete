cf808995f79ba793faa2c4934d249321730d22b9
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-09 13:11:58 +0530

    remove elasti

--- elastidelete.js
+++ elastidelete.js
@@ -1,22 +1,5 @@
-let SL_REDIS = require('slappforge-sdk-redis');
-let clusterManager = require('./ClusterManager');
-const redis = new SL_REDIS.Redis(clusterManager);
-
 exports.handler = async (event) => {
-    // You must always quit the redis client after it's used
-    redis.rename({
-        clusterIdentifier: 'k2',
-        params: [{
-            key: 'k2',
-            name: '2'
-        }]
-    }, function (error, response, redisClient) {
-        if (error) {
-            callback(error);
-        } else {
-            //redisClient.quit();
-        }
-    });
+   
 
     return { "message": "Successfully executed" };
 };
